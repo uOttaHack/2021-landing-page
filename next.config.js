d4122e5e6f1469af771550e84796e255847c2110
Tommy Deng <git@tommydeng.com>
2020-09-28 03:43:10 -0400

    chore(deps): switch to different svg loader plugin
    - replace babel-plugin-inline-react-svg with @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -13,4 +13,15 @@ module.exports = {
   devIndicators: {
     autoPrerender: false,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
