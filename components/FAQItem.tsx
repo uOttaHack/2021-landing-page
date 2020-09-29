1f57d0e0d3b86cfef29ea7b30289eb14bb346c69
Tommy Deng <git@tommydeng.com>
2020-09-28 23:05:38 -0400

    feat: add WIP/barebones FAQ with opening logic
    refs: #6

--- components/FAQItem.tsx
+++ components/FAQItem.tsx
@@ -1,7 +1,42 @@
 import { ReactElement } from "react";
+import theme from "@/src/theme";
+import {
+  AccordionItem,
+  AccordionHeader,
+  AccordionIcon,
+  AccordionPanel,
+  Box,
+} from "@chakra-ui/core";
 
-function FAQItem(): ReactElement {
-  return <></>;
+function FAQItem(props: {
+  question: string;
+  answer: string;
+  currentQuestion: string;
+}): ReactElement {
+  const isOpen = props.question === props.currentQuestion;
+
+  return (
+    <>
+      <AccordionItem
+        isOpen={isOpen}
+        borderLeft={isOpen ? `4px solid ${theme.colors.brand.primary}` : ""}
+        marginLeft={isOpen ? 0 : "4px"}
+      >
+        <AccordionHeader _focus={{}}>
+          <Box flex="1" textAlign="left">
+            {props.question}
+          </Box>
+          <AccordionIcon />
+        </AccordionHeader>
+        <AccordionPanel pb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
+    </>
+  );
 }
 
 export default FAQItem;
--- components/sections/FAQ.tsx
+++ components/sections/FAQ.tsx
@@ -1,7 +1,29 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+import { Accordion, Box } from "@chakra-ui/core";
+import FAQItem from "@/components/FAQItem";
+import { FAQData } from "@/src/data";
 
 function FAQ(): ReactElement {
-  return <></>;
+  const [currentQuestion, setCurrentQuestion] = useState("");
+
+  return (
+    <>
+      <Accordion allowToggle>
+        {FAQData.map((faqItem) => (
+          <Box
+            key={faqItem.question}
+            onClick={() =>
+              setCurrentQuestion(
+                currentQuestion === faqItem.question ? "" : faqItem.question
+              )
+            }
+          >
+            <FAQItem {...faqItem} currentQuestion={currentQuestion} />
+          </Box>
+        ))}
+      </Accordion>
+    </>
+  );
 }
 
 export default FAQ;
