23869db2ab3558b64be60437df5c1be641e57c79
zachlegros <legroszach3921@gmail.com>
2020-09-30 00:55:09 -0400

    Made Hero section fully responsive

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,54 +1,78 @@
 import { ReactElement } from "react";
-import { Body1, Headline1, Link } from "../core/Text";
+import { Body2, Headline3, Link } from "../core/Text";
 import { CTAButton } from "../core/Button";
 import theme from "@/src/theme";
 import MainGraphic from "../svgs/main_graphic.svg";
-import { Grid, Box, Flex } from "@chakra-ui/core";
+import { Box, Flex } from "@chakra-ui/core";
 import Decoration from "../svgs/decoration.svg";
 
 function Hero(): ReactElement {
-  const headlineSizes = [48, 48, 48, 88];
+  const headlineFontSizes = [48, 48, 48, 72];
+  const headlineLineHeights = [0.9, 0.9, 1];
+  const textFontSizes = [14, 14, 16];
 
   return (
     <>
       <section>
-        <Grid
-          h="100vh"
-          marginTop={-theme.navBarOffset}
+        <Flex
+          h={["100%", "100%", "100vh"]}
+          marginTop={[20, 20, -theme.navBarOffset]}
           alignItems="center"
-          autoFlow="row"
-          templateColumns="40% 60%"
+          flexDirection={["column", "column", "row", "row"]}
         >
-          <Box w="100%">
-            <Body1 paddingBottom={4}>February 7-9, 2021</Body1>
-            <Headline1
-              lineHeight={1}
-              fontSize={headlineSizes}
+          <Box w={["100%", "100%", "41%", "37.5%"]}>
+            <Body2
+              paddingBottom={4}
+              fontSize={textFontSizes}
+              color={theme.colors.brand.background_on}
+            >
+              February 7-9, 2021
+            </Body2>
+            <Headline3
+              lineHeight={headlineLineHeights}
+              fontSize={headlineFontSizes}
               color={theme.colors.brand.background_on}
             >
               Explore
-            </Headline1>
-            <Headline1
-              lineHeight={1}
-              fontSize={headlineSizes}
+            </Headline3>
+            <Headline3
+              lineHeight={headlineLineHeights}
+              fontSize={headlineFontSizes}
               color={theme.colors.brand.background_on}
+              whiteSpace="nowrap"
+              zIndex={1}
+              position="relative"
             >
               your limits.
-            </Headline1>
-            <Body1 paddingTop={4}>
+            </Headline3>
+            <Body2
+              paddingTop={4}
+              fontSize={textFontSizes}
+              color={theme.colors.brand.background_on}
+            >
               MLH Member Event · Hosted Virtually Worldwide
-            </Body1>
-            <Flex alignItems="center" marginTop={16}>
+            </Body2>
+            <Flex
+              w="max-content"
+              marginTop={[12, 12, 16]}
+              flexDirection={["column", "column", "row", "row"]}
+              alignItems={["flex-start", "flex-start", "center"]}
+            >
               <CTAButton>Get Started</CTAButton>
-              <Link marginLeft={8}>Sponsor uOttaHack</Link>
+              <Link marginLeft={[0, 0, 8]} marginTop={[12, 12, 0]}>
+                Sponsor uOttaHack
+              </Link>
             </Flex>
           </Box>
-          <MainGraphic preserveAspectRatio="xMinYMin slice" />
-        </Grid>
+          <Box w={["100%", "110%", "59%", "62.5%"]} marginTop={[16, 16, 0]}>
+            <MainGraphic preserveAspectRatio="xMinYMin slice" />
+          </Box>
+        </Flex>
         <Box
           position="absolute"
           left={-theme.decorationOffset.x}
           bottom={-theme.decorationOffset.y}
+          display={["none", "none", "block"]}
         >
           <Decoration />
         </Box>
